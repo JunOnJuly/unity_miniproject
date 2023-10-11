4627bc8c06583ad499d2d3c7dffc7a981ea90cd3
JunOnJuly <wlgnstjd0717@gmail.com>
2023-10-11 11:29:58 +0900

    add base

---
+++ Zombie/Assets/Scripts/AmmoPack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// 총알
+public class AmmoPack : MonoBehaviour, IItem {
+    public int ammo = 30; // 충전할 총알 수
+
+    public void Use(GameObject target) {
+        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
+
+        if (playerShooter != null && playerShooter.gun != null)
+        {
+            playerShooter.gun.ammoRemain += ammo;
+        }
+        Destroy(gameObject);
+    }
+}
